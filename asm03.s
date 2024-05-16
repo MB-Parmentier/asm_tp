f469ab9dbb98b630f33495ac3d7a3dea55785d7f
MB-Parmentier <103027428+MB-Parmentier@users.noreply.github.com>
2024-05-16 08:58:39 +0200

    Create asm03.s

---
+++ asm03.s
@@ -0,0 +1,35 @@
+global _start
+ 
+section .data
+	value: db '1337',10
+ 
+section .text
+ 
+_start:
+ 
+	mov r8,[rsp+16]
+ 
+	mov rax,1
+	mov rdi,0
+	mov rsi,value
+	mov rdx,5
+	syscall
+ 
+	mov al,[r8]
+	cmp al,'4'
+	jne noteq
+ 
+	mov al,[r8+1]
+	cmp al,'2'
+	jne noteq
+ 
+	mov rax,60
+	mov rdi,0
+	syscall
+ 
+noteq:
+ 
+	mov rax,60
+	mov rdi,1
+	syscall
+ 
