13175e4406ccf3345fe473349964f4bc0ece0de5
MB-Parmentier <103027428+MB-Parmentier@users.noreply.github.com>
2024-06-18 15:24:04 +0200

    Update asm02.s

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,48 +1,49 @@
 global _start
- 
-section .data
- 
-	value: dw '1337',10
- 
-section .bss
- 
-	number: resw 10
- 
-section .text
- 
-_start:
- 
-	mov rax,0
-	mov rdi,1
-	mov rsi, number
-	mov rdx,10
-	syscall
- 
-comparison:
- 
-	cmp byte [rsi],'4'
-	jne exitnoteq
- 
-	cmp byte [rsi+1],'2'
-	jne exitnoteq
- 
-	cmp byte [rsi+2],0xa
-	jne exitnoteq
- 
-exit:
 
+section .data
+
+	value: dw '1337',10
+
+section .bss
+
+	number: resw 10
+
+section .text
+
+_start:
+
+	; affichage de 1337 à ajouter
 	mov rax,1
-	mov rdi,1
+	mov rdi,0
 	mov rsi,value
 	mov rdx,5
 	syscall
- 
+
+	mov rax,0
+	mov rdi,0
+	mov rsi, number
+	mov rdx,10
+	syscall
+
+comparison:
+
+	cmp byte [rsi],'4'
+	jne exitnoteq
+
+equal:
+
+	cmp byte [rsi+1],'2'
+	jne exitnoteq
+
+	cmp byte [rsi+2],0xa
+	jne exitnoteq
+
+exit:
 	mov rax,60
 	mov rdi,0
 	syscall
- 
+
 exitnoteq:
 	mov rax,60
 	mov rdi,1
 	syscall
- 
